9006fa192e4910a0de1360f3181af3178af0b2e1
Vincent S. <vincent.silvestri@hotmail.fr>
2025-12-19 17:00:47 +0100

    Add Athena business view: daily weather metrics by city

--- athena/views/weather_daily_metrics_by_city.sql
+++ athena/views/weather_daily_metrics_by_city.sql
@@ -5,6 +5,7 @@ SELECT
     AVG(temperature_c)        AS avg_temperature_c,
     MIN(temperature_c)        AS min_temperature_c,
     MAX(temperature_c)        AS max_temperature_c,
+    
     AVG(humidity)             AS avg_humidity,
     AVG(wind_kmh)             AS avg_wind_kmh,
     SUM(precipitation_mm)     AS total_precipitation_mm
